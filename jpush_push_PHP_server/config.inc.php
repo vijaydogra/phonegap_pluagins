ee667aaddea6cd82c98a960d764748038edabc68
zhengjun <37217911@qq.com>
2013-01-21 16:27:16 +0800

    jpush_php_server
    jpush_php_server

---
+++ jpush_push_PHP_server/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * 极光推送-V2. PHP服务器端
+ * @author 夜阑小雨
+ * @Email 37217911@qq.com
+ * @Website http://www.yelanxiaoyu.com
+ * @version 20130118 
+ */
+	define('DB_HOST', 'localhost');
+	define('DB_USER', '××××'); //数据库用户名
+	define('DB_PWD', '××××'); //数据库密码
+	define('DB_NAME', '××××'); //数据库
+	define('DB_TAB', '××××');//数据表
+	define('DB_CODE','utf8');	
+	define('appkeys','×××××');	//appkey值 极光portal上面提供 
+	define('masterSecret', '××××××');    //API MasterSecert值 极光portal上面提供 
+	define('platform', 'android');    //推送平台
+	 
+ 
+?>
